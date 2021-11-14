bb044a74146caa0b34497f962f0be2cf147ebd57
Laalasa Krishna <76587137+Laalasa@users.noreply.github.com>
2021-11-14 13:00:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,8 +68,6 @@ function cityname(event) {
   city = city.value;
   search(city);
   console.log(city);
-  city = document.querySelector(".form-control");
-  city.innerHTML="";
 }
 
 function showTemp(response) {
